4e939ff670e360652a69c188cc8c672b1850d6f6
LuizGustavoMoreira <luiz@vpeventos.com>
2020-11-18 19:30:09 -0300

    commit

--- src/components/FormPost/index.jsx
+++ src/components/FormPost/index.jsx
@@ -7,8 +7,6 @@ export default function FormPost(){
            <div className="form-input">
                <img width="40" alt="me" height="40" src="https://scontent.fcpq2-1.fna.fbcdn.net/v/t1.0-1/p160x160/120970581_1539214596249813_6404335918860722817_o.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_eui2=AeEzYBOfqkVVI4-XG2Ze_GejgkszHexTlWuCSzMd7FOVa79droN-L-vn3vJwBF6JmhOoiyM3NWOPUILmQgCmpewe&_nc_ohc=oHGjG5CQRnkAX_Z8cK2&_nc_ht=scontent.fcpq2-1.fna&tp=6&oh=668cad3b249585f6740ba83fb81d4dee&oe=5FD62845"/>
                <input placeholder="No que você está pensando?"/>
-
-               
            </div>
            <div style={{paddingLeft: 30,paddingRight: 30}}>
             <hr />
